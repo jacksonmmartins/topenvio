7583e2dc5618c34862fac2b50f0bb94b04b8c11a
Jackson <jacksondesobral@hotmail.com>
2025-10-12 22:17:58 -0300

    Ajuste footer

--- Front End/src/Components/Footer.jsx
+++ Front End/src/Components/Footer.jsx
@@ -4,7 +4,7 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer className="footer">
-      <p>© 2025 MinhaMarca. Todos os direitos reservados.</p>
+      <p>© 2025 Top Envio. Todos os direitos reservados.</p>
     </footer>
   );
 }
